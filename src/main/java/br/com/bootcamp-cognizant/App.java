33a76780bf6557acaa4a64abfc2163e6d7585115
fabricio-pedro <wolfxzero2@gmail.com>
2022-01-03 22:34:46 -0300

    commit inicial do bootcamp Dio da Cognizant

---
+++ src/main/java/br/com/bootcamp-cognizant/App.java
@@ -0,0 +1,13 @@
+package br.com.bootcamp-cognizant;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
